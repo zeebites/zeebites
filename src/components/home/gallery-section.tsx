31f3fda1abebd994ce12ae1a2264160cb4aa830b
Rexedge <ap.oyeniran@gmail.com>
2023-06-24 10:06:10 +0100

    Image preview

--- src/components/home/gallery-section.tsx
+++ src/components/home/gallery-section.tsx
@@ -6,9 +6,23 @@ import BlurImage from '../ui/blur-image';
 const GallerySection: React.FC = () => {
 	const { gallery } = COMPANY_PROFILE;
 	const [selectedFilter, setSelectedFilter] = useState('all');
+	const [selectedImage, setSelectedImage] = useState<null | ImageProps>(
+		null
+	);
 
 	const handleFilterChange = (filter: string) => {
 		setSelectedFilter(filter);
+		setSelectedImage(null); // Reset selected image when changing the filter
+	};
+
+	interface ImageProps {
+		url: string;
+		caption: string;
+		category: string;
+	}
+
+	const handleImageClick = (image: ImageProps) => {
+		setSelectedImage(image);
 	};
 
 	const filteredGallery =
@@ -54,22 +68,44 @@ const GallerySection: React.FC = () => {
 						Events
 					</button>
 				</div>
-				<div className='grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4'>
+				<div className='grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6'>
 					{filteredGallery.map((image, index) => (
-						<div key={index}>
+						<div
+							key={index}
+							className='shadow-md bg-orange-100 overflow-hidden rounded-md hover:scale-105 transition-all duration-300 cursor-pointer'
+							onClick={() => handleImageClick(image)}
+						>
 							<BlurImage
 								src={image.url}
 								alt={image.caption}
-								className='w-full h-full object-cover rounded-md'
+								className='w-full h-60 object-cover'
 								height={500}
 								width={500}
 							/>
-							<p className='text-center mt-2 text-gray-600'>
+							<p className='text-center my-2 text-orange-600'>
 								{image.caption}
 							</p>
 						</div>
 					))}
 				</div>
+				{selectedImage && (
+					<div
+						className='fixed top-0 left-0 z-[210] w-full h-full bg-black bg-opacity-75 flex justify-center items-center p-10'
+						onClick={() => setSelectedImage(null)}
+					>
+						<img
+							src={selectedImage && selectedImage.url}
+							alt={selectedImage.caption}
+							className='max-w-full max-h-full'
+						/>
+						<div
+							className='absolute top-0 right-0 p-5 text-white'
+							onClick={() => setSelectedImage(null)}
+						>
+							CLOSE
+						</div>
+					</div>
+				)}
 			</div>
 		</section>
 	);
